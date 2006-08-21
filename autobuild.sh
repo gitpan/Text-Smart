67594f981b50d92b4ad6bc52a43145849aedb022
Daniel P. Berrangé <dan@berrange.com>
2006-08-21 03:15:23 +0000

    Import of DANBERR/Text-Smart-1.0.1 from CPAN.
    gitpan-cpan-distribution: Text-Smart
    gitpan-cpan-version:      1.0.1
    gitpan-cpan-path:         DANBERR/Text-Smart-1.0.1.tar.gz
    gitpan-cpan-author:       DANBERR
    gitpan-cpan-maturity:     released

---
+++ autobuild.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+NAME="Text-Smart"
+
+set -e
+
+# Make things clean.
+
+make -k realclean ||:
+rm -rf MANIFEST blib
+
+# Make makefiles.
+
+perl Makefile.PL PREFIX=$AUTOBUILD_INSTALL_ROOT
+make manifest
+echo $NAME.spec >> MANIFEST
+
+# Build the RPM.
+make
+
+if [ -z "$USE_COVER" ]; then
+  perl -MDevel::Cover -e '' 1>/dev/null 2>&1 && USE_COVER=1 || USE_COVER=0
+fi
+
+if [ -z "$SKIP_TESTS" -o "$SKIP_TESTS" = "0" ]; then
+  if [ "$USE_COVER" = "1" ]; then
+    cover -delete
+    HARNESS_PERL_SWITCHES=-MDevel::Cover make test
+    cover
+    mkdir blib/coverage
+    cp -a cover_db/*.html cover_db/*.css blib/coverage
+    mv blib/coverage/coverage.html blib/coverage/index.html
+  else
+    make test
+  fi
+fi
+
+
+make install
+
+rm -f $NAME-*.tar.gz
+make dist
+
+if [ -f /usr/bin/rpmbuild ]; then
+  rpmbuild -ta --clean $NAME-*.tar.gz
+fi
--- lib/Text/Smart.pm
+++ lib/Text/Smart.pm
@@ -18,28 +18,29 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: Smart.pm,v 1.2 2004/05/13 10:42:28 dan Exp $
+# $Id: Smart.pm,v 1.3 2004/12/31 16:00:45 dan Exp $
 
 =pod
 
 =head1 NAME
 
-Text::Smart - Processor for 'smarttext' markup
+Text::Smart - Processor for 'smart text' markup
 
 =head1 SYNOPSIS
 
   use Text::Smart;
-  
+
   my $markup = Text::Smart->new(entities => \%entities);
-  
+
   my $text = $markup->process($text, %opts);
-  
+
   my $text = $markup->process_divider($text);
   my $text = $markup->process_itemize($text, %opts);
   my $text = $markup->process_enumeration($text, %opts);
   my $text = $markup->process_paragraph($text, %opts);
   my $text = $markup->process_smart($text, %opts);
-  
+
+  # Virtual methods for subclasses to implement
   my $text = $markup->generate_divider();
   my $text = $markup->generate_itemize(@items);
   my $text = $markup->generate_enumeration(@items);
@@ -48,11 +49,12 @@ Text::Smart - Processor for 'smarttext' markup
   my $text = $markup->generate_italic($text)
   my $text = $markup->generate_monospace($text);
   my $text = $markup->generate_link($text, $url);
+  my $text = $markup->escape($text);
 
 =head1 DESCRIPTION
 
-This module provides an interface for converting 
-smarttext markup into an arbitrary text based markup 
+This module provides an interface for converting
+smarttext markup into an arbitrary text based markup
 language, such as HTML, Latex, or Troff.
 
 =head2 SMARTTEXT MARKUP
@@ -73,7 +75,7 @@ There are six pre-defined entities
   (C) - Insert copyright symbol
   (TM) - Insert trademark symbol
   (R) - Insert registered symbol
-  
+
   1/2 - insert a fraction
   1/4 - insert a fraction
   3/4 - insert a fraction
@@ -124,7 +126,18 @@ To use this options pass them as a named parameter:
 
   $markup->process($text, no_links => 1, no_lists => 1);
 
+=head2 SUBCLASSING
+
+This module provides the basic parsing routines and
+framework for outputting text, however, it must be
+subclassed to generate the markup language specific
+tags. The primary subclass is L<Text::Smart::HTML> which
+is used to generate HTML markup.
+
 =head1 METHODS
+
+The 'generate_XXX' methods are virtual and need to be implemented
+by subclasses.
 
 =over 4
 
@@ -133,272 +146,383 @@ To use this options pass them as a named parameter:
 package Text::Smart;
 
 use strict;
-use Carp qw(confess);
+use warnings;
 
-use vars qw($VERSION);
+our $VERSION = "1.0.1";
 
-$VERSION = "1.0.0";
+=item my $proc = Text::Smart->new();
 
-sub new 
-  {
+Create a new smart text processor. This constructor would
+not normally be called by application code, since this module
+must be sub-classed to be useful. The primary subclass is for
+generating HTML, L<Text::Smart::HTML>.
+
+=cut
+
+sub new {
     my $proto = shift;
     my $class = ref($proto) || $proto;
     my $self = {};
     my %params = @_;
-    
-    $self->{entities} = exists $params{entities} ? $params{entities} : confess "entities parameter is required";
-    
+
     bless $self, $class;
-    
+
     return $self;
-  }
+}
 
+=item my $markup = $proc->process($text)
 
-sub process
-  {
+Converts a piece of smart text, passed in as the parameter into the
+target markup language of this processor. The markedup text is returned
+
+=cut
+
+sub process {
     my $self = shift;
     my $text = shift;
     my %params = @_;
-    
+
     my @blocks = split /\r?\n(\r?\n)+/, (ref($text) ? ${$text} : $text);
 
     foreach (@blocks) {
-      if (/^\s*(___+|---+)\s*$/ && !$params{no_rules}) {
-        $_ = $self->process_divider($_);
-      } elsif (/^\*\s/ && !$params{no_lists}) {
-        $_ = $self->process_itemize($_, @_);
-      } elsif (/^\+\s/ && !$params{no_lists}) {
-        $_ = $self->process_enumeration($_, @_);
-      } elsif (/^&\w+\(.*\)/) {
-	$_ = $self->process_heading($_, @_);
-      } elsif (/\w/) {
-        $_ = $self->process_paragraph($_, @_);
-      }
+	if (/^\s*(___+|---+)\s*$/ && !$params{no_rules}) {
+	    $_ = $self->_process_divider($_);
+	} elsif (/^\*\s/ && !$params{no_lists}) {
+	    $_ = $self->_process_itemize($_, @_);
+	} elsif (/^\+\s/ && !$params{no_lists}) {
+	    $_ = $self->_process_enumeration($_, @_);
+	} elsif (/^&\w+\(.*\)/) {
+	    $_ = $self->_process_heading($_, @_);
+	} elsif (/\w/) {
+	    $_ = $self->_process_paragraph($_, @_);
+	}
     }
-    
+
     return join("\n", @blocks);
-  }
+}
 
 
-sub process_divider
-  {
+sub _process_divider {
     my $self = shift;
     local $_ = shift;
-    
+
     return $self->generate_divider();
-  }
+}
 
 
-sub process_itemize
-  {
+sub _process_itemize {
     my $self = shift;
     local $_ = shift;
 
     my @items = split /^\*\s+/m;
     shift @items if $items[0] eq '';
 
-    return $self->generate_itemize(map { $self->process_smart($_, @_) } @items);
-  }
+    return $self->generate_itemize(map { $self->_process_smart($_, @_) } @items);
+}
 
 
-sub process_enumeration
-  {
+sub _process_enumeration {
     my $self = shift;
     local $_ = shift;
-    
+
     my @items = split /^\+\s*/m;
     shift @items if $items[0] eq '';
-    
-    return $self->generate_enumeration(map { $self->process_smart($_, @_) } @items);
-  }
+
+    return $self->generate_enumeration(map { $self->_process_smart($_, @_) } @items);
+}
 
 
-sub process_heading
-  {
-      my $self = shift;
-      local $_ = shift;
-      
-      $_ =~ /^&(\w+)\((.*)\)/;
-      
-      return $self->generate_heading($2, $1);
-  }
-
-sub process_paragraph
-  {
+sub _process_heading {
     my $self = shift;
     local $_ = shift;
-    
-    return $self->generate_paragraph($self->process_smart($_, @_));
-  }
 
+    $_ =~ /^&(\w+)\((.*)\)/;
 
-sub process_smart
-  {
+    return $self->generate_heading($self->_process_smart($2, @_), $1);
+}
+
+sub _process_paragraph {
     my $self = shift;
     local $_ = shift;
+
+    return $self->generate_paragraph($self->_process_smart($_, @_));
+}
+
+
+sub _process_smart {
+    my $self = shift;
+    local $_ = $self->escape(shift);
     my %params = @_;
-    
+
     my $links = {};
-    
+
     # We're going to use the octal characters \001 and \002 for
     # escaping stuff, so we'd better make sure there aren't any
     # in the text.
     s/\001//g;
     s/\002//g;
     s/\003//g;
-    
+
     unless ($params{no_links}) {
-      # We've got to protect the url of links before we go further,
-      # however we can't actually generate the link yet because
-      # that interferes with the monospace stuff below....
-      s|@@|\001|gx;
-      s|@([^\(@]+)\(([^\)]+)\)|'@' . $1 . '(' . $self->_obscure($2, $links) . ')'|gex;
-      s|\001|@@|gx;
+	# We've got to protect the url of links before we go further,
+	# however we can't actually generate the link yet because
+	# that interferes with the monospace stuff below....
+	s|@@|\001|gx;
+	s|@([^\(@]+)\(([^\)]+)\)|'@' . $1 . '(' . $self->_obscure($2, $links) . ')'|gex;
+	s|\001|@@|gx;
     }
-    
+
     unless ($params{no_symbols}) {
-      # We transform a few common symbols
-      # We don't substitute them straight in because the
-      # substituted text might interfere with stuff that
-      # follows...
-      s|\b1/4\b|"\003fraction14\003"|gex;
-      s|\b1/2\b|"\003fraction12\003"|gex;
-      s|\b3/4\b|"\003fraction34\003"|gex;
-      
-      s|\(C\)|"\003copyright\003"|gex;
-      s|\(R\)|"\003registered\003"|gex;
-      s|\(TM\)|"\003trademark\003"|gex;
+	# We transform a few common symbols
+	# We don't substitute them straight in because the
+	# substituted text might interfere with stuff that
+	# follows...
+	s|\b1/4\b|"\003fraction14\003"|gex;
+	s|\b1/2\b|"\003fraction12\003"|gex;
+	s|\b3/4\b|"\003fraction34\003"|gex;
+
+	s|\(C\)|"\003copyright\003"|gex;
+	s|\(R\)|"\003registered\003"|gex;
+	s|\(TM\)|"\003trademark\003"|gex;
     }
-    
+
     unless ($params{no_links}) {
-      # We protect hyperlinks so that the '/' or '@' doesn't get 
-      # mistaken for a block of italics / link
-      s|([a-z]+://[^\s,\(\)><]*)|'@' . $self->_obscure($1, $links) . '(' . $self->_obscure($1, $links) . ')'|gex;
-      s|(mailto:[^\s,\(\)><]*)|'@' . $self->_obscure($1, $links) . '(' . $self->_obscure($1, $links) . ')'|gex;
+	# We protect hyperlinks so that the '/' or '@' doesn't get
+	# mistaken for a block of italics / link
+	s|([a-z]+://[^\s,\(\)><]*)|'@' . $self->_obscure($1, $links) . '(' . $self->_obscure($1, $links) . ')'|gex;
+	s|(mailto:[^\s,\(\)><]*)|'@' . $self->_obscure($1, $links) . '(' . $self->_obscure($1, $links) . ')'|gex;
     }
-    
+
     unless ($params{no_inline}) {
-      # Next lets process italics /italic/
-      # NB. this must be first, otherwise closing tags </foo>
-      # interfere with the pattern matching
-      s|//|\001|gx;
-      s|(?<!\w)/([^/]+)/(?!\w)|$self->generate_italic($1)|gex;
-      s|\001|/|gx;
-      
-      # Lets process bold text *bold*
-      s|\*\*|\001|gx;
-      s|(?<!\w)\*([^\*]+)\*(?!\w)|$self->generate_bold($1)|gex;
-      s|\001|\*|gx;
-      
-      # Now we're onto the monospace stuff =monospace=
-      s|==|\001|gx;
-      s|(?<!\w)=([^=]+)=(?!\w)|$self->generate_monospace($1)|gex;
-      s|\001|=|gx;
+	# Next lets process italics /italic/
+	# NB. this must be first, otherwise closing tags </foo>
+	# interfere with the pattern matching
+	s|//|\001|gx;
+	s|(?<!\w)/([^/]+)/(?!\w)|$self->generate_italic($1)|gex;
+	s|\001|/|gx;
+
+	# Lets process bold text *bold*
+	s|\*\*|\001|gx;
+	s|(?<!\w)\*([^\*]+)\*(?!\w)|$self->generate_bold($1)|gex;
+	s|\001|\*|gx;
+
+	# Now we're onto the monospace stuff =monospace=
+	s|==|\001|gx;
+	s|(?<!\w)=([^=]+)=(?!\w)|$self->generate_monospace($1)|gex;
+	s|\001|=|gx;
     }
-    
+
     unless ($params{no_links}) {
-      # Links are next on the list @text(url)
-      s|@@|\001|gx;
-      s|@([^\(@]+)\(([^\)]+)\)|$self->generate_link($2, $1)|gex;
-      s|\001|@|gx;
-      
-      # Finally we can unobscure the hyperlinks
-      s|\002([^\002]+)\002|$links->{$1}|gex;
+	# Links are next on the list @text(url)
+	s|@@|\001|gx;
+	s|@([^\(@]+)\(([^\)]+)\)|$self->generate_link($2, $1)|gex;
+	s|\001|@|gx;
+
+	# Finally we can unobscure the hyperlinks
+	s|\002([^\002]+)\002|$links->{$1}|gex;
     }
-    
+
     unless ($params{no_symbols}) {
-      # And those entities
-      s|\003([^\003]+)\003|$self->{entities}->{$1}|gex;
+	# And those entities
+	s|\003([^\003]+)\003|$self->generate_entity($1)|gex;
     }
-    
+
     return $_;
-  }
+}
 
 
-sub _obscure
-  {
+sub _obscure {
     my $self = shift;
     my $link = shift;
     my $map = shift;
-    
+
     my @keys = keys %{$map};
     my $id = $#keys + 1;
-    
+
     $map->{$id} = $link;
 
     return "\002$id\002";
-  }
+}
 
+=item my $markup = $proc->generate_divider()
 
-sub generate_divider
-  {
+Called to generate a horizontal section divider. The generated text must
+be returned in string format. This method must be implemented by subclasses.
+
+=cut
+
+sub generate_divider {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_divider method";
-  }
 
+    die "class " . ref($self) . " did not implement the generate_divider method";
+}
 
-sub generate_itemize
-  {
+=item my $markup = $proc->generate_itemize(@items)
+
+Called to generate an itemized list of bullet points. The (already marked up) text
+for each item is passed as a list of parameters. The generated text must be returned
+in string format. This method must be implemented by subclasses.
+
+=cut
+
+sub generate_itemize {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_itemize method";
-  }
 
+    die "class " . ref($self) . " did not implement the generate_itemize method";
+}
 
-sub generate_enumeration
-  {
+=item my $markup = $proc->generate_enumeration(@items)
+
+Called to generate an itemized list of numbered points. The (already marked up) text
+for each item is passed as a list of parameters. The generated text must be returned
+in string format. This method must be implemented by subclasses.
+
+=cut
+
+sub generate_enumeration {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_enumeration method";
-  }
 
+    die "class " . ref($self) . " did not implement the generate_enumeration method";
+}
 
-sub generate_paragraph
-  {
+=item my $markup = $proc->generate_paragraph($text)
+
+Called to generate a paragraph of text. The (already marked up) text for the body
+of the paragraph is passed in as the only parameter. The generated text must be
+returned in string format. This method must be implemented by subclasses.
+
+=cut
+
+sub generate_paragraph {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_paragraph method";
-  }
 
+    die "class " . ref($self) . " did not implement the generate_paragraph method";
+}
 
-sub generate_bold
-  {
+=item my $markup = $proc->generate_bold($text)
+
+Called to generate bold text. The plain text is passed in as the parameter, and
+the marked up text should be returned in string format. This method must be
+implemented by subclasses.
+
+=cut
+
+sub generate_bold {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_bold method";
-  }
+
+    die "class " . ref($self) . " did not implement the generate_bold method";
+}
 
 
-sub generate_italic
-  {
+=item my $markup = $proc->generate_italic($text)
+
+Called to generate italic text. The plain text is passed in as the parameter, and
+the marked up text should be returned in string format. This method must be
+implemented by subclasses.
+
+=cut
+
+sub generate_italic {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_italic method";
-  }
+
+    die "class " . ref($self) . " did not implement the generate_italic method";
+}
 
 
-sub generate_monospace
-  {
+=item my $markup = $proc->generate_monospace($text)
+
+Called to generate fixed-width text. The plain text is passed in as the parameter, and
+the marked up text should be returned in string format. This method must be
+implemented by subclasses.
+
+=cut
+
+sub generate_monospace {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_monospace method";
-  }
+
+    die "class " . ref($self) . " did not implement the generate_monospace method";
+}
 
 
-sub generate_link
-  {
+
+=item my $markup = $proc->generate_link($url, $text)
+
+Called to generate a hyperlink. The destination of the link is the first parameter,
+and the text being linked is the second parameter. The marked up text must be returned
+in string format. This method must be implemented by subclasses.
+
+=cut
+
+
+sub generate_link {
     my $self = shift;
-    
-    confess "class " . ref($self) . " did not implement the generate_link method";
-  }
 
+    die "class " . ref($self) . " did not implement the generate_link method";
+}
+
+=item my $markup = $proc->generate_entity($name);
+
+Called to generated a special named entity. There are 6 named entities which
+need to be supported:
+
+=over 4
+
+=item fraction12
+
+The fraction 1/2
+
+=item fraction14
+
+The fraction 1/4
+
+=item fraction 34
+
+The fraction 3/4
+
+=item copyright
+
+The copyright symbol
+
+=item trademark
+
+The trademark symbol
+
+=item registered
+
+The rights registered symbol
+
+=back
+
+The markup corresponding to the specified entity must be returned in string
+format.
+
+=cut
+
+sub generate_entity {
+    my $self = shift;
+
+    die "class " . ref($self) . " did not implement the generate_entity method";
+}
+
+=item my $text = $proc->escape($text)
+
+Called to escape any characters which have special meaning in the destination
+markup language. For example, in HTML, this would escape angle brackets and
+the ampersand symbol. The escaped text must be returned in string format.
+
+=cut
+
+sub escape {
+    my $self = shift;
+
+    die "class " . ref($self) . " did not implement the escape method";
+}
 
 1 # So that the require or use succeeds.
 
 __END__
 
-=back 4
+=back
 
 =head1 AUTHORS
 
